73fdd1d3bab509596744a624b3d517bfd8840f48
George Ostrouchov <ostrouchovg@utk.edu>
2019-12-05 12:04:47 -0500

    10x data

--- R/trajectories.R
+++ R/trajectories.R
@@ -110,7 +110,7 @@ trajectories = function(dat, ng, iter = 10, maxdf = 50, plot = FALSE) {
 sessionInfo()
 source("R/generate.R")
 set.seed(90)
-dat = gen_long_data(n_id = 1000, m_obs = 25, e_range = c(365*3, 365*10),
+dat = gen_long_data(n_id = 10000, m_obs = 25, e_range = c(365*3, 365*10),
                     plots = FALSE)
 a = deltime(a, paste0("Data (", paste(dim(dat), collapse = ","), ") generated"))
 
