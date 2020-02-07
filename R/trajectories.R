bd9155e499f9a13e3103aae4d804329182b022ad
George Ostrouchov <ostrouchovg@utk.edu>
2020-02-06 23:37:58 -0500

    scaling experiments via JSON scripts

--- R/trajectories.R
+++ R/trajectories.R
@@ -182,24 +182,33 @@ trajectories = function(dat, ng, group, iter = 15, maxdf = 50, plot = FALSE,
 }
 
 library(jsonlite)
-PL = list(
-  gen_par = list(n_id = 20000, m_obs = 25, e_range = c(365*3, 365*10), plots = FALSE),
-  cores = list(e_mc = 4, m_mc = 4, bam_nthreads = 1, blas = 1),
-  traj_par = list(maxdf = 50, iter = 20, starts = 8, idperstart = 20,
-                  ng_vec = c(2, 3, 4, 5), replicates = 10)
-)
-write_json(PL, "trajectories.par", pretty = TRUE)
-PL = read_json("trajectories.par", simplifyVector = TRUE)
+##
+## Read or create JASON parameter list
+## 
+parname = "trajectories.par" 
+if(file.exists(parname)) {
+  PL = read_json(parname, simplifyVector = TRUE)
+} else {
+  PL = list( # Default parameters
+    gen_par = list(seed = 90, n_id = 20000, m_obs = 25,
+                   e_range = c(365*3, 365*10), plots = FALSE),
+    cores = list(e_mc = 4, m_mc = 4, bam_nthreads = 1, blas = 1),
+    traj_par = list(seed = 79, maxdf = 50, iter = 20, starts = 8, idperstart = 20,
+                    ng_vec = c(2, 3, 4, 5), replicates = 10)
+  )
+  write_json(PL, parname, pretty = TRUE)
+}
 
 source("R/generate.R")
 source("R/scaling.R")
 source("R/evaluate.R")
-set.seed(90)
+set.seed(PL$gen_par$seed)
 
 dat = gen_long_data(n_id = PL$gen_par$n_id, m_obs = PL$gen_par$m_obs,
                     e_range = PL$gen_par$e_range, plots = PL$gen_par$plots)
 a = a_fit = deltime(a, paste0("\nData (", paste(dim(dat), collapse = ","), ") generated"))
 
+set.seed(PL$traj_par$seed)
 cores = PL$cores
 ng_vec = PL$traj_par$ng_vec
 maxdf = PL$traj_par$maxdf
