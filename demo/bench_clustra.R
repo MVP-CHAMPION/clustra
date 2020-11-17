e495d59025fe8352e449905e4e7594708121f0bf
Ostrouchov <ostrouchovg@ornl.gov>
2020-11-17 14:16:11 -0500

    bugs in output

--- demo/bench_clustra.R
+++ demo/bench_clustra.R
@@ -3,28 +3,34 @@ library(clustra)
 #' bench_clustra: Times several core use and data size configurations and
 #' reports the results.
 #' 
+#' OpenBLAS lib gives ~30% improvement on 80K-45 but no multicore improvement.
+#' 
 #' @param nid
 #' Vector of data size configurations using number of id's
 #' 
-bench_clustra = function(nid = c(10000, 20000),
+bench_clustra = function(nid,
+                         e_mc = 1:2, m_mc = 1:2, nthreads =  1:2, blas = 1, reps = 1:2,
     fp = list(maxdf = 30, iter = 8, starts = 4, idperstart = 20, retry_max = 3),
-                         c1 = 2, c2 = 2, c3 =  2, c4 = 2, 
                          verbose = FALSE) {
   rng_prev = RNGkind("L'Ecuyer-CMRG")
   for(n_id in nid) {
-    data = gen_traj_data(n_id, m_obs = 25, s_range = c(-50, -10),
+    set.seed(123473)
+    data = gen_traj_data(n_id, m_obs = 45, s_range = c(-50, -10),
                          e_range = c(3*365, 10*365), reference = 100)
-    results = data.frame(matrix(NA, nrow=length(nid)*c1*c2*c3*c4, ncol = 9))
+    rows = length(nid)*
+      length(e_mc)*length(m_mc)*length(nthreads)*length(blas)*
+      length(reps)
+    results = data.frame(matrix(NA, nrow = rows, ncol = 9))
     names(results) = c("n_id", "rep", "ec", "mc", "bc", "bl", 
                        "time", "xit", "all.equal")
     compare = NULL
-    for(ec in seq(1, c1)) {
-      for(mc in seq(1, c2)) {
-        for(bc in seq(1, c3)) {
-          for(bl in seq(1, c4)) {
-            for(rep in 1:3) {
+    for(ec in e_mc) {
+      for(mc in m_mc) {
+        for(bc in nthreads) {
+          for(bl in blas) {
+            for(rep in reps) {
               openblasctl::openblas_set_num_threads(bl)
-              set.seed(123473)
+              set.seed(1234737)
               cat("starting", n_id, rep, ec, mc, bc, bl, "...")
               time = system.time((
                 cl = clustra(data, k = 3,
@@ -33,7 +39,7 @@ bench_clustra = function(nid = c(10000, 20000),
                              cores = c(e_mc = ec, m_mc = mc, nthreads = bc,
                                        blas = bl), verbose = verbose)
               ))
-              er = paste(xit_report(cl), collapse = " ")
+              er = paste(xit_report(cl, fp), collapse = " ")
 
               if(is.null(compare)) {
                 compare = cl$group
@@ -41,9 +47,13 @@ bench_clustra = function(nid = c(10000, 20000),
               } else {
                 agree = all.equal(compare, cl$group)
               }
-              cat(time["elapsed"], agree, "\n")
+              cat(er, time["elapsed"], agree, "\n")
             
-              ir = (ec - 1)*c2*c3*c4 + (mc - 1)*c3*c4 + (bc - 1)*c4 + bl
+              ir = (ec - 1)*length(m_mc)*length(nthreads)*length(blas)*length(reps) + 
+                (mc - 1)*length(nthreads)*length(blas)*length(reps) + 
+                (bc - 1)*length(blas)*length(reps) +
+                (bl - 1)*length(reps) +
+                rep
               results[ir, ] = data.frame(n_id = n_id, rep = rep,
                                          ec = ec, mc = mc, bc = bc, bl = bl,
                                          time = time["elapsed"], xit = er,
@@ -57,4 +67,4 @@ bench_clustra = function(nid = c(10000, 20000),
   print(results)
 }
 
-bench_clustra(c(10000, 20000), 1, 1, 1, 4)
+bench_clustra(c(80000, 160000), 1:6, 1:6, 1, 1, 1:2)
