3081dc10a4337a75911acf0502009418965d962c
Ostrouchov <ostrouchovg@ornl.gov>
2020-12-02 16:05:33 -0500

    back to tapply

--- demo/bench_clustra_prof.R
+++ demo/bench_clustra_prof.R
@@ -2,9 +2,9 @@
 set.seed(123473)
 data = gen_traj_data(200000, m_obs = 45, s_range = c(-50, -10), e_range = c(3*365, 10*365), reference = 100)
 data$id = as.numeric(factor(data$id))
-set.seed(123473)
+set.seed(12347313)
 Rprof()
-cl = clustra(data, k = 3, fp = list(maxdf = 30, iter = 10, starts = 5, idperstart = 20, retry_max = 3), cores = c(e_mc = 1, m_mc = 1, nthreads = 1, blas = 1), verbose = TRUE)
+cl = clustra(data, k = 3, fp = list(maxdf = 30, iter = 10, starts = 8, idperstart = 20, retry_max = 3), cores = c(e_mc = 6, m_mc = 6, nthreads = 1, blas = 1), verbose = TRUE)
 Rprof(NULL)
 summaryRprof()
 
