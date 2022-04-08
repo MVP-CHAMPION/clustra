37cf7bcf4f538b9233f13dcd2e39d453a2d4e1be
Rachel <rachel.quaden@va.gov>
2022-04-08 13:27:57 -0400

    newdata

--- R/trajectories.R
+++ R/trajectories.R
@@ -370,7 +370,9 @@ trajectories = function(data, k, group, maxdf, conv = c(10, 0), mccores = 1, ver
     ## E-step:
     ##   predict each id's trajectory with each model
     if(verbose) cat(" (E-step ")
-    newdata = force(as.data.frame(data[, list(time, vars)]))
+    varlist = data[,..vars]
+    
+    newdata = force(as.data.frame(data[, .(time,varlist)]))
     myPREDlist = parallel::mclapply(myTPSlist, pred_g, newdata = newdata, 
                               mc.cores = mccores)
     
