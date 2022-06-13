a036f3000171ac75f71060e36432594630bf638f
Rachel <rachel.quaden@va.gov>
2022-06-13 15:22:21 -0400

    remove loop

--- R/generate.R
+++ R/generate.R
@@ -139,21 +139,21 @@ gendata = function(vars, clusters, m_obs, s_range, e_range, min_obs) {
 gen_traj_data = function(vars, clusters, m_obs,  meanlist, curvlist, cvlist, s_range, e_range, min_obs = 3,
                          cv = 0.05, verbose = FALSE)
 {
-  Datalist = rep(list(NULL),length(meanlist))
-  for(q in 1:length(meanlist)){
+#  Datalist = rep(list(NULL),length(meanlist))
+#  for(q in 1:length(meanlist)){
   if(is.numeric(clusters)) {
     if(length(clusters) == 1) {
       n_id = 2^(1:clusters)*500
     } else { n_id = clusters }
     d = length(vars)*length(n_id)
     
-    means=matrix(meanlist[[q]],nrow=length(n_id),ncol=length(vars),byrow=TRUE)
+    means=matrix(meanlist,nrow=length(n_id),ncol=length(vars),byrow=TRUE)
     #means = matrix(runif(d, 40, 220), nrow = length(n_id))
     #curv = matrix(sample(1:3, d, replace = TRUE), 
     #              nrow = length(n_id), ncol = length(vars))
-    curv =matrix(curvlist[[q]],nrow=length(n_id),ncol=length(vars),byrow=TRUE)
+    curv =matrix(curvlist,nrow=length(n_id),ncol=length(vars),byrow=TRUE)
     #noise_cv = rep(cv, length(n_id))
-    noise_cv = matrix(cvlist[[q]],nrow=length(n_id),ncol=2,byrow=TRUE)
+    noise_cv = matrix(cvlist,nrow=length(n_id),ncol=2,byrow=TRUE)
     
     clusters = cbind(n_id, noise_cv, means, curv)
     colnames(clusters) = c("n_id", paste0("noise_cv", 1:length(vars)), paste0("mean", 1:length(vars)),
@@ -170,8 +170,8 @@ gen_traj_data = function(vars, clusters, m_obs,  meanlist, curvlist, cvlist, s_r
   
   id_list = gendata(vars, clusters, m_obs, s_range, e_range, min_obs)
   id_mat = do.call(rbind, id_list)
-  Datalist[[q]]=data.table::data.table(id_mat)
-  }
-  return(Datalist)
+  Datalist=data.table::data.table(id_mat)
+  #}
+ # return(Datalist)
 }
                                               
